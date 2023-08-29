9c79ae619718f08ffff133c94f5df50f05f6f69e
Guru Prasadh <officialmailforguru@gmail.com>
2023-08-29 22:52:11 +0530

    u

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { TableComponent } from './table/table.component';
-import { EmployeeDetailsService } from './employee-details.service';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, TableComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
-  providers: [EmployeeDetailsService],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
