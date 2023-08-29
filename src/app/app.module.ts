391d3c31fdca028ecf95f9f50bd1c26323af3f12
Guru Prasadh <officialmailforguru@gmail.com>
2023-08-29 21:14:43 +0530

    Upload A Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TableComponent } from './table/table.component';
+import { EmployeeDetailsService } from './employee-details.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, TableComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
+  providers: [EmployeeDetailsService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/employee.ts
@@ -0,0 +1,14 @@
+export interface EmployeeVivaramTypes {
+  brand: string;
+  category: string;
+  description: string;
+  images: Array<string>;
+  id: number;
+  imageUrl: string;
+  title: string;
+  discountPercentage: number;
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+}
