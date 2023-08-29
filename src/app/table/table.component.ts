8dfdc6349221025405693c55613dbe076b9f4895
Guru Prasadh <officialmailforguru@gmail.com>
2023-08-29 23:02:25 +0530

    msg

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeDetailsService } from '../employee-details.service';
+import { EmployeeVivaramTypes } from 'src/employee';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
-export class TableComponent {
-
+export class TableComponent implements OnInit {
+  employeeVivaram: Array<EmployeeVivaramTypes> = [];
+  images: Array<EmployeeVivaramTypes> = [];
+  red = 'blue';
+  constructor(private employeeInfo: EmployeeDetailsService) {}
+  ngOnInit(): void {
+    this.employeeInfo
+      .passData()
+      .subscribe((data: any) => (this.employeeVivaram = data.products));
+  }
 }
